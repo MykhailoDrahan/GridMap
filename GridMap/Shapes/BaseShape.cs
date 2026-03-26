c588564f283934075dc20fa3d6d01d48cceef36c
mykhailodrahan <mykhailodrahan@gmail.com>
2026-03-26 11:30:34 +0200

    feat: add BaseShape class

---
+++ GridMap/Shapes/BaseShape.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GridMap.Shapes
+{
+    internal abstract class BaseShape
+    {
+        public Guid Id { get; init; } = Guid.NewGuid();
+        public List<PointF> Vertices { get; } = new List<PointF>();
+
+        protected BaseShape() { }
+        public BaseShape(List<PointF> vertices)
+        {
+            Vertices = vertices;
+        }
+    }
+}
