b01011916231e0c362cca527ff8584deb8b0d225
mykhailodrahan <mykhailodrahan@gmail.com>
2026-04-02 12:18:52 +0300

    feat: add ShapeRenderer class for drawing shapes

---
+++ GridMap/ShapeRenderer.cs
@@ -0,0 +1,27 @@
+﻿using GridMap.Shapes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GridMap
+{
+    internal class ShapeRenderer
+    {
+        public Brush Brush { get; set; } = Brushes.Black;
+        public float Thickness { get; set; } = 1.0f;
+        public ShapeRenderer() { }
+
+        public void DrawShape(Graphics g, BaseShape shape)
+        {
+            using (Pen pen = new Pen(Brush, Thickness)) 
+            {
+                int count = shape.Vertices.Count;
+
+                for (int i = 0; i < count; i++)
+                {
+                    g.DrawLine(pen, shape.Vertices[i], shape.Vertices[(i + 1) % count]);
+                }                
+            }
+        }
+    }
+}
